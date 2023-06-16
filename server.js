0ed99dea36589f9afa1d988ca5a4dfadfbe2ef40
Daudu Arunegbode charles <blaqwayschannel@gmail.com>
2023-06-16 13:20:05 +0100

    replace cors origin

--- server.js
+++ server.js
@@ -22,7 +22,7 @@ app.use(express.urlencoded({extended:false}));
 app.use(methodOverride('_method'));
 app.use(cors({
   credentials: true,
-  origin: "https://trendspace.onrender.com",
+  origin: "*",
  
 }));
 app.use(fileUpload());
