9ae2770df2da71c30c5407e974255a3bab86ca07
Daudu Arunegbode charles <blaqwayschannel@gmail.com>
2024-01-25 13:09:26 +0100

    removed the mongo .env variable

--- dbconnection/connectDB.js
+++ dbconnection/connectDB.js
@@ -3,7 +3,7 @@ const mongoose   = require("mongoose");
 
 const connectToDb= async()=>{
     try {
-        await mongoose.connect(process.env.MONGO_DB_STRING);
+        await mongoose.connect('mongodb+srv://charleeblaq:charleeblaq1994@cluster0.nh33n48.mongodb.net/readmeDB');
         console.log('database connected successfully')
     } catch (error) {
         console.error(error.message);
